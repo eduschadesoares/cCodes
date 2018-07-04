5490b1449c026e84076d7df84fe9c4b67bf47f8c
Eduardo <eduschadesoares@hotmail.com>
2018-07-04 03:37:15 -0300

    gitignore

---
+++ Paginação/Trabalho 4 - Paginacao.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <clocale>
+#include <cstdio>
+#include <ctime>
+
+#define p 512 //               paginas
+#define f 256 //               frames
+#define by 8192 //             8kb
+#define maxEnd 4194304 //      4MB
+
+using namespace std;
+
+int n;
+int pag[p], frame[f] = {}, pagUsadas[f], enderecos[maxEnd];
+
+void print(const std::string& str) {
+    for (size_t i = 0; i != str.size(); ++i) { 
+        std::cout << str[i];
+        usleep(20000);
+    }
+}
+
+void erro(int val){
+	print("Erro:");
+}
+/////////////////////////////////////
+
+bool confereSeJaFoiUsado(int valor){
+	for (int i=0; i<f; i++){
+		if (pagUsadas[i] == valor) return true; // p�gina já foi utilizada
+	}
+	return false; // página livre
+}
+
+void randomizaPaginas(){
+	for (int i = 0; i < f; i++){
+		frame[i] = i;
+	}
+	for (int i = 0; i < f; i++) {    // whoop hahah mistureba
+	    int temp = frame[i];
+	    int random = rand() % f;
+	    frame[i] = frame[random];
+	    frame[random] = temp;
+	}
+}
+
+void init(){
+	for (int i = 0; i < maxEnd; i++){
+		enderecos[i] = -1;
+	}
+	
+	srand(time(NULL));
+	for(int i=0; i<p; i++){ //isso nem precisa na vdd
+		pag[i] = i;
+	}
+	randomizaPaginas();
+}
+
+void goodbye(){
+	system("cls");
+	exit(1);
+}
+
+void criarEnderecos(){
+	print("Insira a quantidade de endereços: ");
+	cin >> n;
+	
+	if(n > maxEnd) erro(2);
+	else if (n < 0) erro(1);
+	else if (n == 0) goodbye();
+	else {
+		for (int i = 0; i < n; i++) {    // whoop hahah mistureba
+		    enderecos[i] = rand() % maxEnd;
+		}	
+	}
+	
+	for (int i=0; i<n; i++){
+		printf(" %d, ", enderecos[i]);
+	}
+}
+
+void paginacao(){
+	int m = 0, valor = 0, paginaX = 0, frameX = 0, deslocamento = 0;
+	while (m < n){
+	
+	// meu irmao, � aqui que a parada complica 
+		for (int i = 0; i<n; i++){
+			valor = enderecos[i];
+			paginaX = valor / by;	
+		}
+	}
+}
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	init();
+	criarEnderecos();
+	paginacao();
+
+
+	return 1;
+}
+
+
