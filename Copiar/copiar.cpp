0b1b8acde4d579efc02530be8f9f6b39820a1b62
Eduardo <eduschadesoares@hotmail.com>
2018-08-21 21:53:51 -0300

    First step

--- Copiar/copiar.cpp
+++ Copiar/copiar.cpp
@@ -6,15 +6,16 @@
 #include <time.h>
 #include <sys/wait.h>
 
+// Constantes
 const int b = 1;
 const int Kb = 1024;
 const int Mb = 1024 * 1024;
 const int Gb = 1024 * 1024 * 1024;
 
+// Protótipos
+void menu();
+
+// The very beginning
 int main() {
-    printf("%i\n", b);
-    printf("%i\n", Kb);
-    printf("%i\n", Mb);
-    printf("%i\n", Gb);
-    return 0;
+    menu();
 }
