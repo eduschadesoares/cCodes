dac4320de5b13625a808e0db5b99be2263cc212c
Eduardo <eduschadesoares@hotmail.com>
2018-08-21 21:13:49 -0300

    First one

---
+++ Copiar/copiar.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <time.h>
+#include <sys/wait.h>
+
+int main() {
+    return 0;
+}
